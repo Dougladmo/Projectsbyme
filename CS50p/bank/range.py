d4d0f4a315cbed0b537e2d3830019c1b044ed406
Douglas Oliveira <100490271+Dougladmo@users.noreply.github.com>
2023-06-02 05:33:30 -0300

    Delete CS50p directory

--- CS50p/bank/range.py
+++
@@ -1,6 +0,0 @@
-def value():
-    g = input("").strip().upper()
-    if ((g[0] + g[1] + g[2] + g[3] + g[4]) == 'HELLO'):
-        print("ok")
-
-value()
--- CS50p/einstein/einstein.py
+++
@@ -1,5 +0,0 @@
-def main():
-    m = int(input("m: "))
-    E = m * (300000000 * 300000000)
-    print(f"E: {E}")
-main()
