bb83ee4695884f9477b8a488b7604e967843d57f
Douglas Oliveira <100490271+Dougladmo@users.noreply.github.com>
2023-08-10 16:43:25 -0300

    Delete Monkey Pizza directory

--- Monkey Pizza/DjangoPizza/mysite/reservas/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.reservas, name='reserva'),
-    path('valida_reserva', views.valida_reserva, name='valida_reserva'),
-]
