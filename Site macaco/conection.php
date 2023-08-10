6a986900b06de48bf3d893e830e4e5d566fee09b
Douglas Oliveira <100490271+Dougladmo@users.noreply.github.com>
2023-08-10 16:43:42 -0300

    Delete Site macaco directory

--- Site macaco/conection.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', '127.0.0.1');
-define('USUARO', 'root');
-define('SENHA', '123')
-define('DB', 'login')
-
-$conexao = mysqli-connection(HOST, USUARIO, DB) or die ('Não foi possivel conectar';)
